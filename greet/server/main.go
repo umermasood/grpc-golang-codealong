241a6382559a86069f9cbd9209da408578cbea4f
Umer Masood <umermasood.dev@gmail.com>
2022-11-07 15:20:27 +0500

    server setup boilerplate

---
+++ greet/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	pb "github.com/umermasood/grpc-golang-codealong/greet/proto"
+)
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+var addr = "0.0.0.0:50051"
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to server %v", err)
+	}
+
+}
