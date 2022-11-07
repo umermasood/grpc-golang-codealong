7fea2e9bae82d2ae5a76f875bb573a5cbbd82f3f
Umer Masood <umermasood.dev@gmail.com>
2022-11-07 16:50:51 +0500

    client setup boilerplate

---
+++ greet/client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("Failed to close connection: %v", err)
+		}
+	}(conn)
+	//...
+}
