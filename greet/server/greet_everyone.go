77bcbdb44a320445c317f1797f2385d588fa9cf3
Umer Masood <umermasood.dev@gmail.com>
2022-11-12 12:24:28 +0500

    08 - bidirectional streaming api server implementation

---
+++ greet/server/greet_everyone.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/umermasood/grpc-golang-codealong/greet/proto"
+)
+
+func (s *Server) GreetEveryone(stream pb.GreetService_GreetEveryoneServer) error {
+	log.Println("GreetEveryone was invoked")
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v\n", err)
+		}
+
+		res := "Hello" + req.FirstName + "!"
+		if err = stream.Send(&pb.GreetResponse{
+			Result: res,
+		}); err != nil {
+			log.Fatalf("Error while sending response to client: %v\n", err)
+		}
+	}
+}
