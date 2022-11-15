bbcbaadb5e398a38232f62d0fe42bf0bdf2d09f4
Umer Masood <umermasood.dev@gmail.com>
2022-11-15 12:53:26 +0500

    10 - CreateBlog rpc endpoint server implementation

---
+++ blog/server/create.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+
+	pb "github.com/umermasood/grpc-golang-codealong/blog/proto"
+)
+
+func (s *Server) CreateBlog(ctx context.Context, in *pb.Blog) (*pb.BlogId, error) {
+	log.Printf("CreateBlog was invoked with: %v\n", in)
+
+	data := BlogItem{
+		AuthorId: in.AuthorId,
+		Title:    in.Title,
+		Content:  in.Content,
+	}
+
+	res, err := collection.InsertOne(ctx, data)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Internal Error: %v\n", err))
+	}
+
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+
+	if !ok {
+		return nil, status.Errorf(codes.Internal, "Cannot convert to OID")
+	}
+
+	return &pb.BlogId{
+		Id: oid.Hex(),
+	}, nil
+}
---
+++ blog/server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	pb "github.com/umermasood/grpc-golang-codealong/blog/proto"
+)
+
+type Server struct {
+	pb.BlogServiceServer
+}
+
+var collection *mongo.Collection
+var addr = "0.0.0.0:50051"
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:root@localhost:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("blogdb").Collection("blog")
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+
+	s := grpc.NewServer()
+	pb.RegisterBlogServiceServer(s, &Server{})
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
